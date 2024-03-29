9d702808b99b46737c8f67697fe02eb36fd6ad2f
Ifeanyi Michael <ifmichael24@gmail.com>
2024-03-29 19:08:51 +0100

    Completed Create feed function

--- resources/views/livewire/user/feed.blade.php
+++ resources/views/livewire/user/feed.blade.php
@@ -1,28 +1,24 @@
 <x-feed-layout>
-    <div class="drawer lg:drawer-open">
-    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content flex flex-col items-center justify-center dark:text-slate-200">
-        <!-- Page content here -->
-        <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-       <div class="grid columns-12 grid-flow-col gap-4 px-4">
+    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+       <div class="grid lg:columns-12 grid-flow-col gap-4 px-4">
             <!-- feed section -->
-            <div class="col-span-7 border-slate-300 relative">
+            <div class="lg:col-span-7 border-slate-300 relative">
                 <x-story-slide class="overflow-hidden" style="width:100%;"/>
                 
             </div>
             <!-- Suggestions section -->
-            <div class="col-span-5">
+            <div class="lg:col-span-5">
                 <x-follower-suggestions/>
 
             </div>
        </div>
-    </div> 
-    <div class="drawer-side px-4 w-12 border-opacity-5 border-e-2 border-gray-400">
-        <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label> 
-        
-        <!-- Sidebar content here -->
-        <x-sidebar/>
-    
-    </div>
-    </div>
+
+       @if(session()->has('success'))
+        @push('scripts')
+            <script>
+                $.notify("{{session()->get('success')}}",'success')
+            </script>
+            {{session()->remove('success')}}
+        @endpush
+       @endif
 </x-feed-layout>
