1ef3354be0c216ecfbe37eb94d92dcc1decaa993
Ifeanyi Michael <ifmichael24@gmail.com>
2024-04-06 11:25:00 +0100

    Changed our story-slider and suggested followers to iterate

--- resources/views/components/story-slide.blade.php
+++ resources/views/components/story-slide.blade.php
@@ -1,73 +1,18 @@
 @php
 $faker = Faker\Factory::create();
+$storyCount = 30;
+$unreadStories = [2,5,4,6,1,8,10,6,7,15,28,30,25,28,18];
 @endphp
 <div {{$attributes->merge(['class'=>"relative"])}}>
     <div class="carousel carousel-center rounded-box">
-        <div class="story carousel-item flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-                <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-2"/>
+         
+        @for($x=1; $x <= $storyCount; $x++)
+            <div class="{{ (in_array($x,$unreadStories)) ? 'story' : '' }} carousel-item flex-col px-2 mx-3 w-20">
+                <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
+                    <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-{{$x}}"/>
+                </div>
+                <p class="truncate block">{{$faker->name}}</p>
             </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div> 
-        <div class="story carousel-item flex flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-            <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-10"/>
-            </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div> 
-        <div class="story carousel-item flex flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-                <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-01"/>
-            </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div> 
-        <div class="carousel-item flex flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-                <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-3"/>
-            </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div> 
-        <div class="story carousel-item flex flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-            <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-11"/>
-            </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div>
-        <div class="story carousel-item flex flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-                <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-4"/>
-            </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div>
-        <div class="carousel-item flex flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-                <img class="w-full h-full rounded-full" src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Drink" />
-            </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div> 
-        <div class="carousel-item flex flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-                <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-5"/>
-            </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div> 
-        <div class="carousel-item flex flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-                <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-6"/>
-            </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div> 
-        <div class="carousel-item flex flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-                <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-7"/>
-            </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div> 
-        <div class="carousel-item flex flex-col px-2 mx-3 w-20">
-            <div class="carousel-img w-20 h-20 rounded-full overflow-hidden">
-                <x-avater class="rounded-full w-full h-full" src="https://source.unsplash.com/random/500x500/?face-8"/>
-            </div>
-            <p class="truncate block">{{$faker->name}}</p>
-        </div>
+        @endfor
     </div>
 </div>
