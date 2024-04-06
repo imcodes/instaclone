fc46bf71db51161576c10625d6f68951775e74b3
Ifeanyi Michael <ifmichael24@gmail.com>
2024-04-06 11:25:00 +0100

    Changed our story-slider and suggested followers to iterate

--- resources/views/components/follower-suggestions.blade.php
+++ resources/views/components/follower-suggestions.blade.php
@@ -1,5 +1,9 @@
 @php
 $faker = Faker\Factory::create();
+
+//Just for fake follower purpose
+$followerCount = 10;
+$followMsg = ['Suggested for you', 'Follows you','Popular', 'Friends']
 @endphp
 <div class="flex flex-col gap-3">
 
@@ -8,34 +12,18 @@ $faker = Faker\Factory::create();
         <a href="#" class="text-right">See All</a>
     </div>
 
-    <div class="flex flex-row justify-center items-center gap-3">
-        <x-avater src="https://source.unsplash.com/random/500x500/?face-09" styles="rounded-full w-10 h-10"/>
-        <div class="grid grid-flow-col gap-3 text-sm space-x-8 w-full">
-            <p>
-                <strong>{{$faker->name}}</strong>
-                <span class="text-xs text-gray-400 block">Suggested for you</span>
-            </p>
-            <a href="#" class="text-blue-500 text-right"> Follow</a>
+    @for($x = 1; $x <= $followerCount; $x++)
+        @php $img = mt_rand(100,200) @endphp
+        <div class="flex flex-row justify-center items-center gap-3">
+            <x-avater src="https://source.unsplash.com/random/500x500/?face-{{$img}}" styles="rounded-full w-10 h-10"/>
+            <div class="grid grid-flow-col gap-3 text-sm space-x-8 w-full">
+                <p>
+                    <strong>{{$faker->name}}</strong>
+                    <span class="text-xs text-gray-400 block">{{$followMsg[mt_rand(1,4) - 1]}}</span>
+                </p>
+                <a href="#" class="text-blue-500 text-right"> Follow</a>
+            </div>
         </div>
-    </div>
-    <div class="flex flex-row justify-center items-center gap-3">
-        <x-avater src="https://source.unsplash.com/random/500x500/?face-12" styles="rounded-full w-10 h-10"/>
-        <div class="grid grid-flow-col gap-3 text-sm space-x-8 w-full">
-            <p>
-                <strong>{{$faker->name}}</strong>
-                <span class="text-xs text-gray-400 block">Follows you</span>
-            </p>
-            <a href="#" class="text-blue-500 text-right"> Follow</a>
-        </div>
-    </div>
-    <div class="flex flex-row justify-center items-center gap-3 m">
-        <x-avater src="https://source.unsplash.com/random/500x500/?face-13" styles="rounded-full w-10 h-10"/>
-        <div class="grid grid-flow-col gap-3 text-sm space-x-8 w-full">
-            <p>
-                <strong>{{$faker->name}}</strong>
-                <span class="text-xs text-gray-400 block">Suggested for you</span>
-            </p>
-            <a href="#" class="text-blue-500 text-right"> Follow</a>
-        </div>
-    </div>
+    @endfor
+    
 </div>
