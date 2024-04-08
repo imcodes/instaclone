2389c77fa07f446ea34f1b3cf27a60076dc47d42
Ifeanyi Michael - imcodes <ifnotmichael24@gmail.com>
2024-04-08 23:04:50 +0100

    Updated the forgotten password view with the instgram logo

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,7 +1,8 @@
 <x-guest-layout>
     <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
+    <x-slot name="logo">
+            <!-- <x-authentication-card-logo /> -->
+            <x-instagram-logo />
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
