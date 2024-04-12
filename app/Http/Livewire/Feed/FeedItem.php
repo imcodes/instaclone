2df1f13be99dde8bbe0500a07dda838a56e3e06b
Ifeanyi Michael <ifmichael24@gmail.com>
2024-04-12 19:22:38 +0100

    Added the comment Modal template

--- app/Http/Livewire/Feed/FeedItem.php
+++ app/Http/Livewire/Feed/FeedItem.php
@@ -8,6 +8,12 @@ class FeedItem extends Component
 {
 
     public $post;
+    public $showingComments = false;
+
+    public $mediaExtensions = [
+        'image' => ['jpg','jpeg','gif','webp','png'],
+        'video' => ['mp4']
+    ];
     public function render()
     {
         return view('livewire.feed.feed-item');
