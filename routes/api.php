1a09e326c135b6bc426e77e43112204acb207297
Ifeanyi Michael <ifmichael24@gmail.com>
2024-04-18 19:18:06 +0100

    Started Api Development with Laravel

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/with-comment',[PostController::class,'indexWithComments'])->name('indexWithRelation');
+
+Route::prefix('post')->group(function(){
+    Route::get('/{limit?}',[PostController::class,'index'])->name('index');
+    Route::post('/',[PostController::class,'store'])->name('store');
+
+    Route::get('/{id}',[PostController::class,'show'])->name('show-post');
+    Route::patch('/{id}',[PostController::class,'update'])->name('show-post');
 });
